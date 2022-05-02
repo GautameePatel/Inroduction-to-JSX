6914234300cf36c17b316d2750263a56a5ab74cf
GautameePatel <99380053+GautameePatel@users.noreply.github.com>
2022-05-02 16:24:29 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+ReactDOM.render(
+  <div>
+    <h1>I am Learning</h1>
+    <ul>
+      <li>JSX</li>
+      <li>Babbel</li>
+      <li>React</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
